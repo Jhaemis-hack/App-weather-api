edb55c4cdae0d5d9eaeed6a3fbd00b159dddfa0f
Jhaemis-hack <gbemilekekenny@gmail.com>
2025-02-10 00:34:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const axios = require('axios')
+const morgan = require('morgan')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 4000;
+const reqIp = require('request-ip')
+
+//middlewares config
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(reqIp.mw())
+
+app.use((req, res)=> {
+    const ip = req.clientIp;
+    res.end(ip);
+});
+
+
+app.listen(port, (err) =>{
+    if(err) console.log(err)
+    console.log(`app is listening @ http://localhost:${port}`)
+})
